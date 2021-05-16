0e60ee69e5e3bb85c29596b04b2d4a1f4c9c5c77
iyed-dev <79877352+iyed-dev@users.noreply.github.com>
2021-05-16 17:34:44 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    TOKEN: "TOKEN DU BOT",
+    PREFIX: "LE PREFIX",
+    OWNER: "ID DU CREATEUR",
+    STARTCHANNEL: "SALON OU ON ANNONCE LE DEMRAGE DU BOT",
+    allbotlogs: "SALON LOGS",
+}
+module.exports = config;
